74b1aa56f0fb0d83669797884b68d1ba3d6c4fb5
rbuler <rafalbuler@gmail.com>
2024-03-03 19:35:31 +0100

    Added n-step TD function

--- utils.py
+++ utils.py
@@ -110,3 +110,55 @@ def td(env,
 
         V_track[e] = V
     return V, V_track
+
+
+def ntd(env,
+        pi=None,
+        gamma=1.0,
+        init_alpha=0.5,
+        min_alpha=0.01,
+        alpha_decay_ratio=0.3,
+        n_step=3,
+        n_episodes=500):
+    
+    nS = env.observation.n
+    V = np.zeros(nS)
+    V_track = np.zeros((n_episodes, nS))
+    alphas = decay_schedule(init_alpha, min_alpha,
+                            alpha_decay_ratio, n_episodes)
+    discounts = np.logspace(0, n_step+1, num=n_step+1,
+                            base=gamma, endpoint=False)
+    
+    for e in tqdm(range(n_episodes)):
+        truncated = False
+        terminated = False
+        state, info = env.reset(seed=42)
+        path = []
+        while not (truncated or terminated) or path is not None:
+            path = path[1:]
+            while not (truncated or terminated) and len(path) < n_step:
+                if pi is not None:
+                    action = pi(state)  # policy action select
+                else:
+                    action = env.action_space.sample()  # random action sample
+                next_state, reward, terminated, truncated, info = env.step(action)
+                experience = (state, action, reward, next_state, terminated, truncated)
+                path.append(experience)
+                state = next_state
+                if terminated or truncated:
+                    break
+            n = len(path)
+            est_state = path[0][0]
+            rewards = np.array(path)[:,2]
+            partial_return = discounts[:n] * rewards
+            bs_val = discounts[-1] * V[next_state] * (1 - terminated)
+
+            ntd_target = np.sum(np.append(partial_return, bs_val))
+            ntd_error = ntd_target - V[est_state]
+            V[est_state] = V[est_state] + alphas[e] * ntd_error
+
+            if len(path) == 1 and path[0][4]:  # if only terminal state in path then
+                path = None                    # set path to none to break episode loop
+
+        V_track[e] = V
+    return V, V_track
