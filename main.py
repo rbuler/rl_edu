762c0f80f15550a42532b9a3c1cdaf947f68c02c
rbuler <rafalbuler@gmail.com>
2024-03-14 14:26:27 +0100

    Added if __name__ == '__main__' idiom

--- main.py
+++ main.py
@@ -4,45 +4,50 @@ from comet_ml.integration.gymnasium import CometLogger
 # from utils import *
 
 
-experiment = Experiment(
-  project_name="rl-edu",
-  workspace="rafalbuler"
-)
+def main():
 
-mode = 'show-only'
+    experiment = Experiment(
+    project_name="rl-edu",
+    workspace="rbuler"
+    )
 
-if mode == 'show-only':
-    render_mode = 'human'
-    env = gym.make("LunarLander-v2", render_mode=render_mode)
-    env = CometLogger(env, experiment)
-    experiment.add_tag(mode)
+    mode = 'show-only'
 
-elif mode == 'save-only':
-    render_mode = 'rgb_array'
-    disable_logger = False
-    env = gym.make("LunarLander-v2", render_mode=render_mode)
-    env = gym.wrappers.RecordVideo(env, 'rendered_videos', disable_logger=disable_logger)
-    env = CometLogger(env, experiment)
-    experiment.add_tag(mode)
+    if mode == 'show-only':
+        render_mode = 'human'
+        env = gym.make("LunarLander-v2", render_mode=render_mode)
+        env = CometLogger(env, experiment)
+        experiment.add_tag(mode)
 
-episodes = 5
+    elif mode == 'save-only':
+        render_mode = 'rgb_array'
+        disable_logger = False
+        env = gym.make("LunarLander-v2", render_mode=render_mode)
+        env = gym.wrappers.RecordVideo(env, 'rendered_videos', disable_logger=disable_logger)
+        env = CometLogger(env, experiment)
+        experiment.add_tag(mode)
 
-test_metric = 0
-experiment.log_metric ('test_int', test_metric)
+    episodes = 2
 
-# generate_trajectory(env=env, pi=None, max_steps=1000)
-
-for e in range(episodes):
-    observation, info = env.reset(seed=42)
-    truncated = False
-    terminated = False
-    while not (truncated or terminated):
-        action = env.action_space.sample()
-        observation, reward, terminated, truncated, info = env.step(action)
-        env.render()
-
-    test_metric = e
+    test_metric = 0
     experiment.log_metric ('test_int', test_metric)
 
+    # generate_trajectory(env=env, pi=None, max_steps=1000)
 
-env.close()
+    for e in range(episodes):
+        observation, info = env.reset(seed=42)
+        truncated = False
+        terminated = False
+        while not (truncated or terminated):
+            action = env.action_space.sample()
+            observation, reward, terminated, truncated, info = env.step(action)
+            env.render()
+        test_metric = e
+        experiment.log_metric ('test_int', test_metric)
+    
+    env.close()
+
+
+
+if __name__ == '__main__':
+    main()
