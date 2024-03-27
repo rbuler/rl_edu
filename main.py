73b4fa753fee46ea421242cc9af7d98017f31c03
rbuler <rafalbuler@gmail.com>
2024-03-27 18:50:50 +0100

    minor changes

--- main.py
+++ main.py
@@ -1,50 +1,38 @@
 import gymnasium as gym
 from comet_ml import Experiment
 from comet_ml.integration.gymnasium import CometLogger
-# from utils import *
+from utils import mc_prediction, td, ntd, td_lambda
 
 
 def main():
-    experiment = Experiment(
-        project_name="rl-edu",
-        workspace="rbuler"
-    )
-
+    # environment = 'LunarLander-v2'
+    environment = 'FrozenLake-v1'
     mode = 'show-only'
 
     if mode == 'show-only':
+        experiment = None
         render_mode = 'human'
-        env = gym.make("LunarLander-v2", render_mode=render_mode)
-        env = CometLogger(env, experiment)
-        experiment.add_tag(mode)
+        env = gym.make(environment, render_mode=render_mode)
+        # env = CometLogger(env, experiment)
+        # experiment.add_tag(mode)
 
     elif mode == 'save-only':
+        experiment = Experiment(
+            project_name="rl-edu",
+            workspace="rbuler"
+        )
         render_mode = 'rgb_array'
         disable_logger = False
-        env = gym.make("LunarLander-v2", render_mode=render_mode)
+        env = gym.make(environment, render_mode=render_mode)
         env = gym.wrappers.RecordVideo(env, 'rendered_videos', disable_logger=disable_logger)
         env = CometLogger(env, experiment)
         experiment.add_tag(mode)
 
-    episodes = 2
-
     test_metric = 0
-    experiment.log_metric('test_int', test_metric)
-
-    # generate_trajectory(env=env, pi=None, max_steps=1000)
-
-    for e in range(episodes):
-        observation, info = env.reset(seed=42)
-        truncated = False
-        terminated = False
-        while not (truncated or terminated):
-            action = env.action_space.sample()
-            observation, reward, terminated, truncated, info = env.step(action)
-            env.render()
-        test_metric = e
+    if experiment is not None:
         experiment.log_metric('test_int', test_metric)
 
-    env.close()
+    V, V_track = td(env, n_episodes=5)
 
 
 if __name__ == '__main__':
