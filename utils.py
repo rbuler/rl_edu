dee9b511211f5332aabbff5d38065a30adb7c959
rbuler <rafalbuler@gmail.com>
2024-05-13 18:01:28 +0200

    Minor bug fixes

--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
-import tqdm
+from tqdm import tqdm
 import numpy as np
 
 def decay_schedule(init_value, min_value,
@@ -20,28 +20,28 @@ def decay_schedule(init_value, min_value,
     return values
 
 
-# def generate_trajectory(env, pi=None, max_steps=20):
+def generate_trajectory_mc_pred(env, pi=None, max_steps=20):
 
-#     trajectory = []
-#     truncated = False
-#     terminated = False
-#     state, info = env.reset(seed=42)
+    trajectory = []
+    truncated = False
+    terminated = False
+    state, info = env.reset(seed=42)
 
-#     while not (truncated or terminated):
-#         if pi is not None:
-#             action = pi(state)  # policy action select
-#         else:
-#             action = env.action_space.sample()  # random action sample
+    while not (truncated or terminated):
+        if pi is not None:
+            action = pi(state)  # policy action select
+        else:
+            action = env.action_space.sample()  # random action sample
         
-#         next_state, reward, terminated, truncated, info = env.step(action)
-#         experience = (state, action, reward, next_state, terminated, truncated)
-#         trajectory.append(experience)
+        next_state, reward, terminated, truncated, info = env.step(action)
+        experience = (state, action, reward, next_state, terminated, truncated)
+        trajectory.append(experience)
 
-#         if len(trajectory) >= max_steps:
-#             trajectory = []
-#             break
-#         state = next_state
-#     return trajectory
+        if len(trajectory) >= max_steps:
+            trajectory = []
+            break
+        state = next_state
+    return np.array(trajectory, np.object_)
 
 
 def generate_trajectory(select_action, Q,
@@ -70,7 +70,7 @@ def generate_trajectory(select_action, Q,
             break
 
         state = next_state
-    return np.array(trajectory, np.object)
+    return np.array(trajectory, np.object_)
 
 
 # Prediction algorithms
@@ -96,7 +96,7 @@ def mc_prediction(env,
     V_track = np.zeros((n_episodes, nS))
 
     for e in tqdm(range(n_episodes)):
-        trajectory = generate_trajectory(env, pi, max_steps)
+        trajectory = generate_trajectory_mc_pred(env, pi, max_steps)
         visited = np.zeros(nS, dtype=np.bool_)
         
         for t, (state, _, reward, _, _, _) in enumerate(trajectory):
@@ -105,7 +105,7 @@ def mc_prediction(env,
             visited[state] = True
             n_steps = len(trajectory[t:])
             G = np.sum(discounts[:n_steps] * trajectory[t:, 2])
-            V[state] = V[state] + alphas * (G - V[state])
+            V[state] = V[state] + alphas[e] * (G - V[state])
 
         V_track[e] = V
     return V.copy(), V_track
@@ -155,7 +155,7 @@ def ntd(env,
         n_step=3,
         n_episodes=500):
     
-    nS = env.observation.n
+    nS = env.observation_space.n
     V = np.zeros(nS)
     V_track = np.zeros((n_episodes, nS))
     alphas = decay_schedule(init_alpha, min_alpha,
@@ -207,7 +207,7 @@ def td_lambda(env,
               lambda_=0.3,
               n_episodes=500):
     
-    nS = env.observation.n
+    nS = env.observation_space.n
     V = np.zeros(nS)
     V_track = np.zeros((n_episodes, nS))
     E = np.zeros(nS)
@@ -229,7 +229,7 @@ def td_lambda(env,
             td_target = reward + gamma * V[next_state] * (1 - terminated)
             td_error = td_target - V[state]
             E[state] = E[state] + 1
-            V = V + alphas * td_error * E
+            V = V + alphas[e] * td_error * E
             E = gamma * lambda_ * E
             state = next_state
         V_track[e] = V
